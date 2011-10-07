af9cf291224295a0d5a4c22a7ae5e8abbe1742fa
thomas alexander <tomalex0@gmail.com>
2011-10-07 17:26:28 +0530

    Updated Model destroy function to make success and failure funciton working

--- override.js
+++ override.js
@@ -1,34 +1,41 @@
 Ext.override(Ext.data.Model, {
-	destroy: function(options) {
-		var me = this,
-			action = 'destroy';
-			options = options || {};
-			Ext.apply(options, {
-				records: [me],
-				action: action
-			});
+    destroy: function(options) {
+        
+        var me = this,
+            action = 'destroy';
+            options = options || {};
+            Ext.apply(options, {
+                records: [me],
+                action: action
+            });
 
-		var operation = new Ext.data.Operation(options),
-			successFn = options.success,
-			failureFn = options.failure,
-			callbackFn = options.callback,
-			scope = options.scope;
-		var callback = function(operation) {
-			if (operation.wasSuccessful()) {
-				if (typeof successFn == 'function') {
-					successFn.call(scope, record, operation);
-				}
-			} else {
-				if (typeof failureFn == 'function') {
-					failureFn.call(scope, record, operation);
-				}
-			}
 
-			if (typeof callbackFn == 'function') {
-				callbackFn.call(scope, record, operation);
-			}
-		};
-		me.getProxy()[action](operation, callback, me);
-		return me;
-	}
+        var operation = new Ext.data.Operation(options),
+            successFn = options.success,
+            failureFn = options.failure,
+            callbackFn = options.callback,
+            scope = options.scope,
+            record;
+            
+        var callback = function(operation) {
+            record = operation.getRecords()[0];
+            if (operation.wasSuccessful()) {
+                if (typeof successFn == 'function') {
+                    successFn.call(scope, record, operation);
+                }
+            } else {
+                if (typeof failureFn == 'function') {
+                    failureFn.call(scope, record, operation);
+                }
+            }
+
+
+            if (typeof callbackFn == 'function') {
+                callbackFn.call(scope, record, operation);
+            }
+        };
+        
+        me.getProxy()[action](operation, callback, me);
+        return me;
+    }
 });
